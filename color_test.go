4495ec408b52a402fc8d39175879dcd55552f66b
DazFather <the6daz6@gmail.com>
2023-08-07 19:21:46 +0200

    Add doc example for color related utilities

---
+++ color_test.go
@@ -0,0 +1,61 @@
+package brush_test
+
+import (
+	"fmt"
+
+	"github.com/DazFather/brush"
+)
+
+/* ---[ EXAMPLES ]--- */
+
+func ExampleRGB() {
+	pink := brush.RGB(
+		brush.MaxIntensity,
+		brush.MediumIntensity,
+		brush.HightIntensity,
+	)
+
+	fmt.Println(brush.Paint(0, &pink, "Flamingo"))
+	// Output: [38;5;0;48;5;218mFlamingo[0m
+}
+
+func ExampleGrayScale() {
+	var (
+		white     = brush.GrayScale(brush.MaxGrayScale)
+		lightGray = brush.GrayScale(brush.MaxGrayScale - 5)
+		gray      = brush.GrayScale(brush.MaxGrayScale / 2)
+
+		myBrush = brush.New(brush.Black.ToExtended(), nil)
+	)
+
+	myBrush.UseBgColor(white).Println("Sunny")
+	myBrush.UseBgColor(lightGray).Println("Cloudy")
+	myBrush.UseBgColor(gray).Println("Rainy")
+
+	// Output:
+	// [38;5;0;48;5;15mSunny
+	// [0m[38;5;0;48;5;251mCloudy
+	// [0m[38;5;0;48;5;243mRainy
+	// [0m
+}
+
+func ExampleUseColor() {
+	selectedBg := brush.UseColor(brush.Magenta)
+
+	fmt.Println(brush.Paint(brush.White, selectedBg, "Magenta"), "is cool")
+	// Output: [37;45mMagenta[0m is cool
+}
+
+func ExamplePickColor() {
+	myBrush := brush.New(brush.Yellow, nil)
+	reversed := brush.New(
+		brush.PickColor(myBrush.Background, brush.Black),
+		brush.UseColor(myBrush.Foreground),
+	)
+
+	myBrush.Println("default brush example")
+	reversed.Println("reversed brush example")
+	// Output: [33mdefault brush example
+	// [0m[30;43mreversed brush example
+	// [0m
+}
