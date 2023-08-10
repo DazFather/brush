20204c71e045d827725444ad8a67faa96a0eadd7
DazFather <the6daz6@gmail.com>
2023-08-10 13:21:26 +0200

    Add  doc example for brush related utility

---
+++ brush_test.go
@@ -0,0 +1,72 @@
+package brush_test
+
+import (
+	//"fmt"
+
+	"github.com/DazFather/brush"
+)
+
+func ExampleBrush_UseFontColor() {
+	myBrush := brush.New(brush.Black, brush.UseColor(brush.White))
+
+	myBrush.Println("something")
+	myBrush.UseFontColor(brush.Blue).Println("something else")
+	// Output:
+	// [30;47msomething
+	// [0m[34;47msomething else
+	// [0m
+}
+
+func ExampleBrush_UseBgColor() {
+	myBrush := brush.New(brush.Black, brush.UseColor(brush.White))
+
+	myBrush.Println("something")
+	myBrush.UseBgColor(brush.Cyan).Println("something else")
+	// Output:
+	// [30;47msomething
+	// [0m[30;46msomething else
+	// [0m
+}
+
+func ExampleBrush_UseBgTransparent() {
+	myBrush := brush.New(brush.Black, brush.UseColor(brush.White))
+
+	myBrush.Println("something")
+	myBrush.UseBgTransparent().Println("something else")
+	// Output:
+	// [30;47msomething
+	// [0m[30msomething else
+	// [0m
+}
+
+func ExampleBrush_Swap() {
+	myBrush := brush.New(brush.Black, brush.UseColor(brush.White))
+
+	myBrush.Println("something")
+	myBrush.Swap().Println("something else")
+	// Output:
+	// [30;47msomething
+	// [0m[37;40msomething else
+	// [0m
+}
+
+func ExampleBrush_Print(values ...any) {
+	myBrush := brush.New(brush.Black, brush.UseColor(brush.White))
+
+	myBrush.Print("Hello", " World")
+	// Output: [30;47mHello World[0m
+}
+
+func ExampleBrush_Println(values ...any) {
+	myBrush := brush.New(brush.Black, brush.UseColor(brush.White))
+
+	myBrush.Println("Hello", "World")
+	// Output: [30;47mHello World[0m
+}
+
+func ExampleBrush_Printf(model string, values ...any) {
+	myBrush := brush.New(brush.Black, brush.UseColor(brush.White))
+
+	myBrush.Printf("%s %s", "Hello", "World")
+	// Output: [30;47mHello World[0m
+}
--- highlight_test.go
+++ highlight_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"regexp"
 	"strings"
+	"testing"
 
 	"github.com/DazFather/brush"
 )
@@ -45,4 +46,76 @@ func ExampleBrush_HighlightFunc() {
 		strings.ToUpper,
 	))
 	// Output: this is [33mUPPERCASE[0m and yellow
+}
+
+func ExampleHighlighted_Append() {
+	var (
+		marker = brush.New(brush.Black, brush.UseColor(brush.Yellow))
+		h      = marker.Highlight("Hello world!", regexp.MustCompile("Hello"))
+	)
+
+	fmt.Print(h.Append(" ", marker.Paint("Hi"), " everyone!"))
+	// Output: [30;43mHello[0m world! [30;43mHi[0m everyone!
+}
+
+func ExampleBrush_Embed() {
+	var (
+		green  = brush.New(brush.Green, nil)
+		blue   = brush.New(brush.Blue, nil)
+		yellow = brush.New(brush.Yellow, nil)
+	)
+
+	fmt.Print(blue.Embed(
+		yellow.Highlight("Sun is yellow\n", regexp.MustCompile(`Sun|yellow`)),
+		green.Paint("Grass"), " is ", green.Paint("green"),
+		"\nAll the rest is blue",
+	))
+	// Output:
+	// [33mSun[0m[34m is [0m[33myellow[0m[34m
+	// [0m[32mGrass[0m[34m is [0m[32mgreen[0m[34m
+	// All the rest is blue[0m
+}
+
+/* ---[ TESTS ]--- */
+
+func TestBrush_Highlight(t *testing.T) {
+	var (
+		rgx    = regexp.MustCompile(`red`)
+		marker = brush.New(brush.Red, nil)
+	)
+
+	assert(t, `Highlighting: "red"`,
+		marker.Highlight("trashredgarbage", rgx).String(),
+		"trash[31mred[0mgarbage",
+	)
+
+	assert(t, `Highlighting: "red banana"`,
+		marker.Highlight("trash red banana trash red lolred", rgx).String(),
+		"trash [31mred[0m banana trash [31mred[0m lol[31mred[0m",
+	)
+}
+
+func TestHighlighted_Append(t *testing.T) {
+	var (
+		rgx    = regexp.MustCompile(`red( \w+)?`)
+		marker = brush.New(brush.Red, nil)
+	)
+
+	banana := marker.Highlight("trash red banana", rgx)
+	assert(t, `Append to "banana"`,
+		banana.Append(" trash").String(),
+		"trash [31mred banana[0m trash",
+	)
+
+	test := marker.Highlight("trashredgarbage", rgx)
+	assert(t, `Append`,
+		test.Append(
+			cool{},
+			"ciao",
+			brush.Paint(brush.Green, nil, "green"),
+			3,
+			&banana,
+		).String(),
+		"trash[31mred[0mgarbagecoolciao[32mgreen[0m3trash [31mred banana[0m trash",
+	)
 }
